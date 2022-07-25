2d6b4771742a26dfed9090d80fda0a83bc4eecf9
Lisa <lisa.das9374@gmail.com>
2022-07-26 00:41:45 +0530

    Refactore: added another method in linked list

--- src/com/linklist/MyLinkedList.java
+++ src/com/linklist/MyLinkedList.java
@@ -19,25 +19,39 @@ public class MyLinkedList<K> {
     		tail=tempNode;
     	}
     	}
+    
+    public void addFirst(INode<K> node) {
+        INode<K> tempNode = node;
+        if (head == null) {
+            head = node;
+            tail = node;
+        }
+        else{
+            tempNode.setNext(head);
+            head = tempNode;
+        }
+    }
+    public void addMiddle(INode<K> node) {
+        INode<K> newNode = node;
+        INode<K> oldNode = node;
+
+        if (head == null) {
+            head = node;
+            tail = node;
+        }
+        else{
+            oldNode= head.getNext();
+            head.setNext(newNode);
+            newNode.setNext(oldNode);
+        }
+    }
+    public void printList(){
+        System.out.println("MyNode "+head);
+    }
     	
     	public void printlist() {
     		
     	System.out.println("MyNode is: "+head);
     }   
     
-
-//    public void add(INode<K> node){
-//        INode<K> tempNode = node;
-//        if (head == null){
-//            head = tempNode;
-//            tail = tempNode;
-//        }
-//        else {
-//            tail.setNext(tempNode);
-//            tail = tempNode;
-//        }
-//    }
-//    public void printList(){
-//        System.out.println("MyNodes :-  "+head);
-//    }
 }
